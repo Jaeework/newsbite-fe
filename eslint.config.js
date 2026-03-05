0b1010aa72c6d95569e11ef8a0bec2bb0a0c2cd9
Jaeework <jaeework@gmail.com>
2026-03-05 16:44:39 +0900

    chore: add husky and lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
-import unusedImports from "eslint-plugin-unused-imports"
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
   globalIgnores(["dist"]),
