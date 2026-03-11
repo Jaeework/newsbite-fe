519a1629af58d0072b58b576f3675130f7b14d11
Jaeework <jaeework@gmail.com>
2026-03-11 18:51:22 +0900

    refactor: #13 InputWithMessage 메세지 배열 형태로 받도록 수정

--- src/components/ui/input-with-message/InputWithMessage.types.ts
+++ src/components/ui/input-with-message/InputWithMessage.types.ts
@@ -2,7 +2,11 @@ import type { InputWithIconProps } from "../input-with-icon/InputWithIcon.types"
 
 export type ValidationVariant = "default" | "error" | "success";
 
+export type ValidationMessage = {
+  message: string;
+  variant: ValidationVariant;
+};
+
 export type InputWithMessageProps = InputWithIconProps & {
-  message?: string;
-  variant?: ValidationVariant;
+  messages?: ValidationMessage[];
 };
